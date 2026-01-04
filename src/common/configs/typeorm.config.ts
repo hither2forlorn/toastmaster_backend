051ddef9e08e7c90082a6b4bfe9ade35763ebdff
Bishwash <bishwash312@gmail.com>
2026-01-04 11:40:51 +0545

    feat: migration configuration added

---
+++ src/common/configs/typeorm.config.ts
@@ -0,0 +1,18 @@
+import 'tsconfig-paths/register';
+import { DataSource } from 'typeorm';
+import { config } from 'dotenv';
+import { join } from 'path';
+
+config();
+
+export default new DataSource({
+  type: 'postgres',
+  url: process.env.DATABASE_URL,
+  entities: [join(__dirname, '../../**/*.entity{.ts,.js}')],
+  migrations: [join(__dirname, '../../migrations/*{.ts,.js}')],
+  ssl: {
+    rejectUnauthorized: false,
+  },
+  synchronize: false,
+  logging: false,
+});
