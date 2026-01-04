677b58641c7737784bc7b9b32e7befae86ea7b2f
Bishwash <bishwash312@gmail.com>
2026-01-04 11:49:12 +0545

    fix: auth module error in production fixed removed the duplicated auth service and reimplemented the previous way of handling user authentication uninstall all the strategies and jwt atuh guard causing error

--- src/app.module.ts
+++ src/app.module.ts
@@ -15,7 +15,6 @@ import { SharedModule } from './common/modules/shared.module';
 import { AgendaTemplateModule } from './modules/agenda-template/agenda-template.module';
 import { AgendaModule } from './modules/agenda/agenda.module';
 import { AgendaReportModule } from './modules/agenda-report/agenda-report.module';
-import { JwtAuthGuard } from './common/guards/jwt-auth.guard';
 
 // Detect environment
 const isProduction = process.env.NODE_ENV === 'production';
@@ -37,7 +36,7 @@ const isProduction = process.env.NODE_ENV === 'production';
     MeetingModule,
     AgendaTemplateModule,
     AgendaModule,
-    AgendaReportModule
+    AgendaReportModule,
   ],
   providers: [
     {
@@ -52,10 +51,6 @@ const isProduction = process.env.NODE_ENV === 'production';
       provide: 'APP_GUARD',
       useClass: JwtGuard,
     },
-    {
-      provide: 'APP_GUARD',
-      useClass: JwtAuthGuard,
-    }
   ],
 })
 export class AppModule {}
