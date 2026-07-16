de3929ff1a0b0a4f92928743942fcf597403afee
shreedhar01 <shreedharkalamagar@gmail.com>
2026-07-16 14:22:58 +0545

    fix: migration ;289

--- src/migrations/1784200000000-AddMemberIdToUser.ts
+++ src/migrations/1784200000000-AddMemberIdToUser.ts
@@ -2,16 +2,19 @@ import { MigrationInterface, QueryRunner, TableColumn } from 'typeorm';
 
 export class AddMemberIdToUser1784200000000 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.addColumn(
-      'users',
-      new TableColumn({
-        name: 'member_id',
-        type: 'varchar',
-        length: '20',
-        isNullable: true,
-        default: null,
-      }),
-    );
+    const hasColumn = await queryRunner.hasColumn('users', 'member_id');
+    if (!hasColumn) {
+      await queryRunner.addColumn(
+        'users',
+        new TableColumn({
+          name: 'member_id',
+          type: 'varchar',
+          length: '20',
+          isNullable: true,
+          default: null,
+        }),
+      );
+    }
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
